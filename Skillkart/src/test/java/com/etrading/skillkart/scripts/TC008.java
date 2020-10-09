ea676a92a7002d8b4e9c65dae6e91b3666d6213e
syed mansoor <syed mansoor@LAPTOP-U9974I6L>
2020-10-09 19:59:46 +0530

    TC008

---
+++ Skillkart/src/test/java/com/etrading/skillkart/scripts/TC008.java
@@ -0,0 +1,14 @@
+package com.etrading.skillkart.scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC008 {
+
+	@Test
+	public void test()
+	{
+		Reporter.log("test",true);
+	}
+	
+}
